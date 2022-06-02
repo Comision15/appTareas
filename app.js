fc4eb429fecd713b6f5c6d07c8c2c96ff4da482c
Comision15 <comision15formar@gmail.com>
2022-06-02 19:45:34 -0300

    show todo done

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const {leerJSON} = require('./modulos/tareas');
 
 switch (argv[2]) {
     case "listar":
-        leerJSON()
+        let tareas = leerJSON();
+        tareas.forEach(tarea => {
+            console.log(`(${tarea.id}) ${tarea.title} - ${tarea.state}`);
+        });
         break;
 
     default:
